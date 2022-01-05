fbcbb2970a16ff2d74bdc17bf5ee96ca13188651
dhundup24 <dhundup24@gmail.com>
2022-01-05 14:28:23 -0700

    removed unessesary comments

--- View/DrawingPanel.cs
+++ View/DrawingPanel.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 namespace WinterBreak2021
 {
     public class DrawingPanel : Panel
-    {     
+    {
         public string drawThisBackground;
 
 
@@ -27,130 +27,142 @@ namespace WinterBreak2021
 
         //logo without arch
         Image gameLogo3 = Image.FromFile(@"..\..\..\Resources\Images\PokemonRockPaperScissorsLogo2.png");
+
         Image howToPlayBackground = Image.FromFile(@"..\..\..\Resources\Images\HowToPlayBackground.png");
-
-
-
-
-
+        Image homeButton = Image.FromFile(@"..\..\..\Resources\Images\homeButton.png");
 
 
         public DrawingPanel()
         {
             this.DoubleBuffered = true;
+            
+
+        }
+
+        void MyButtonHandler(object sender, EventArgs e)
+        {
         }
 
 
 
-        public void drawBackground(object o, PaintEventArgs e)
-        {
-            int width = 900;
-            int height = 690;
-            e.Graphics.DrawImage(background, 0, 0, width, height);
-            e.Graphics.DrawImage(gameLogo1, 240, 150, 400, 170);
-            e.Graphics.DrawImage(gameLogo2, 110, 300, 680, 160);
 
-        }
+        public void drawHomeBackground(object o, PaintEventArgs e)
+    {
+        int width = 900;
+        int height = 690;
+        e.Graphics.DrawImage(background, 0, 0, width, height);
+        e.Graphics.DrawImage(gameLogo1, 240, 150, 400, 170);
+        e.Graphics.DrawImage(gameLogo2, 110, 300, 680, 160);
 
-        public void drawBattleScreen(object o, PaintEventArgs e)
-        {
-            
-            
-            int width = 900;
-            int height = 690;
-            e.Graphics.DrawImage(battleScreen, 0, 0, width, height);
-            charizard = resizeImage(charizard, new Size(300, 280));
-            CreateGUIButton(o, e, charizard, 0, 200 );
+    }
 
-            venusaur = resizeImage(venusaur, new Size(300, 280));
-            CreateGUIButton(o, e, venusaur, 300, 200);
+    public void drawBattleScreen(object o, PaintEventArgs e)
+    {
 
-            blastoise = resizeImage(blastoise, new Size(300, 280));
-            CreateGUIButton(o, e, blastoise, 600, 200);
+
+        int width = 900;
+        int height = 690;
+        e.Graphics.DrawImage(battleScreen, 0, 0, width, height);
+        charizard = resizeImage(charizard, new Size(300, 280));
+        CreateGUIButton(o, e, charizard, 0, 200, new Size(270, 270));
+
+        venusaur = resizeImage(venusaur, new Size(300, 280));
+        CreateGUIButton(o, e, venusaur, 300, 200, new Size(270, 270));
+
+        blastoise = resizeImage(blastoise, new Size(300, 280));
+        CreateGUIButton(o, e, blastoise, 600, 200, new Size(270, 270));
+
+        homeButton = resizeImage(homeButton, new Size(100, 100));
+        CreateGUIButton(o, e, homeButton, 0, 600, new Size(70, 70));
+
+
         }
 
         public void drawHowToPlayScreen(object o, PaintEventArgs e)
-        {
-            int width = 900;
-            int height = 690;
-            e.Graphics.DrawImage(howToPlayBackground, 0, 0, width, height);
+    {
+        int width = 900;
+        int height = 690;
+        e.Graphics.DrawImage(howToPlayBackground, 0, 0, width, height);
 
-            drawInstructions(e);
+        homeButton = resizeImage(homeButton, new Size(100, 100));
+        CreateGUIButton(o, e, homeButton, 0, 630, new Size(70, 70));
+
+        drawInstructions(e);
+    }
+
+
+    public static Image resizeImage(Image imgToResize, Size size)
+    {
+        return (Image)(new Bitmap(imgToResize, size));
+    }
+
+
+    public void CreateGUIButton(object sender, EventArgs e, Image i, int x, int y, Size size)
+    {
+        Button newButton = new Button();
+
+        newButton.Image = i;
+        newButton.Size = size;
+        newButton.Location = new Point(x, y);
+        this.Controls.Add(newButton);
+
+    }
+
+
+    /// <summary>
+    /// onPaint tells the drawingPanel which background to draw
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        if (drawThisBackground == "homeScreen")
+        {
+            drawHomeBackground(background, e);
         }
 
-
-        public static Image resizeImage(Image imgToResize, Size size)
+        else if (drawThisBackground == "battleScreen")
         {
-            return (Image)(new Bitmap(imgToResize, size));
+            drawBattleScreen(battleScreen, e);
         }
 
-
-        public void CreateGUIButton(object sender, EventArgs e, Image i, int x, int y)
+        else if (drawThisBackground == "howToPlayScreen")
         {
-            Button newButton = new Button();
-            
-            newButton.Image = i;
-            newButton.Size = new Size(270, 270);
-            newButton.Location = new Point(x, y);
-            this.Controls.Add(newButton);
-
+            drawHowToPlayScreen(howToPlayBackground, e);
         }
 
+        base.OnPaint(e);
+    }
 
-        /// <summary>
-        /// onPaint tells the drawingPanel which background to draw
-        /// </summary>
-        /// <param name="e"></param>
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            if (drawThisBackground == "homeScreen")
-            {
-                drawBackground(background, e);
-            }
+    /// <summary>
+    /// draws all the instructions for how to play the game in different textboxes containing strings.
+    /// </summary>
+    /// <param name="e"></param>
+    private void drawInstructions(PaintEventArgs e)
+    {
+        // Create string to draw.
+        String drawString = "How To Play: Pokemon Rock Paper Scissors";
 
-            else if(drawThisBackground == "battleScreen")
-            {
-                drawBattleScreen(battleScreen, e);
-            }
+        // Create font and brush.
+        Font drawFont3 = new Font("Arial", 16);
+        SolidBrush drawBrush = new SolidBrush(Color.Black);
 
-            else if (drawThisBackground == "howToPlayScreen")
-            {
-                drawHowToPlayScreen(howToPlayBackground, e);
-            }
+        // Create rectangle for drawing.
+        float x = 200.0F;
+        float y = 0.0F;
+        float width1 = 500.0F;
+        float height1 = 50.0F;
+        RectangleF drawRect = new RectangleF(x, y, width1, height1);
 
-            base.OnPaint(e);
-        }
+        // Draw rectangle to screen.
+        Pen blackPen = new Pen(Color.Black);
+        e.Graphics.DrawRectangle(blackPen, x, y, width1, height1);
 
-        /// <summary>
-        /// draws all the instructions for how to play the game in different textboxes containing strings.
-        /// </summary>
-        /// <param name="e"></param>
-        private void drawInstructions(PaintEventArgs e)
-        {
-            // Create string to draw.
-            String drawString = "How To Play: Pokemon Rock Paper Scissors";
+        // Set format of string.
+        StringFormat drawFormat = new StringFormat();
+        drawFormat.Alignment = StringAlignment.Center;
 
-            // Create font and brush.
-            Font drawFont3 = new Font("Arial", 16);
-            SolidBrush drawBrush = new SolidBrush(Color.Black);
-
-            // Create rectangle for drawing.
-            float x = 200.0F;
-            float y = 0.0F;
-            float width1 = 500.0F;
-            float height1 = 50.0F;
-            RectangleF drawRect = new RectangleF(x, y, width1, height1);
-
-            // Draw rectangle to screen.
-            Pen blackPen = new Pen(Color.Black);
-            e.Graphics.DrawRectangle(blackPen, x, y, width1, height1);
-
-            // Set format of string.
-            StringFormat drawFormat = new StringFormat();
-            drawFormat.Alignment = StringAlignment.Center;
-
-            // Draw string to screen.
-            e.Graphics.DrawString(drawString, drawFont3, drawBrush, drawRect, drawFormat);
+        // Draw string to screen.
+        e.Graphics.DrawString(drawString, drawFont3, drawBrush, drawRect, drawFormat);
 
 
 
@@ -158,79 +170,79 @@ namespace WinterBreak2021
 
 
 
-            // Create string to draw.
-            String drawString1 = "FOR PEOPLE WHO DON'T KNOW WHAT POKEMON IS OR HOW BASIC POKEMON TYPES WORK:" + '\n' +
-                                 "1. Pokemon is a game where you capture pokemon in the wild and raise your own." + '\n' +
-                                 "    Through battling with other pokemon, your pokemon will level up and will soon evolve." + '\n' +
-                                 "2. Every Pokemon has a certain type (fire, water, grass, etc.)." + '\n' +
-                                 "    Some Pokemon of a certain type have an advantage over another pokemon's type." + '\n' +
-                                 "    For example, Water type pokemon are strong against fire (and fire is weak to water)." + '\n' +
-                                 "    Grass pokemon are strong against water types (and water is weak to grass)" + '\n' +
-                                 "    Lastly, Fire pokemon are strong against grass types (and grass is weak to fire)" + '\n' +
-                                 "    For this game, you only need to know your type matchups with grass, fire and water.";
+        // Create string to draw.
+        String drawString1 = "FOR PEOPLE WHO DON'T KNOW WHAT POKEMON IS OR HOW BASIC POKEMON TYPES WORK:" + '\n' +
+                             "1. Pokemon is a game where you capture pokemon in the wild and raise your own." + '\n' +
+                             "    Through battling with other pokemon, your pokemon will level up and will soon evolve." + '\n' +
+                             "2. Every Pokemon has a certain type (fire, water, grass, etc.)." + '\n' +
+                             "    Some Pokemon of a certain type have an advantage over another pokemon's type." + '\n' +
+                             "    For example, Water type pokemon are strong against fire (and fire is weak to water)." + '\n' +
+                             "    Grass pokemon are strong against water types (and water is weak to grass)" + '\n' +
+                             "    Lastly, Fire pokemon are strong against grass types (and grass is weak to fire)" + '\n' +
+                             "    For this game, you only need to know your type matchups with grass, fire and water.";
 
 
-            // Create font and brush.
-            Font drawFont1 = new Font("Arial", 16);
-            SolidBrush drawBrush2 = new SolidBrush(Color.Black);
+        // Create font and brush.
+        Font drawFont1 = new Font("Arial", 16);
+        SolidBrush drawBrush2 = new SolidBrush(Color.Black);
 
-            // Create rectangle for drawing.
-            float x1 = 0.0F;
-            float y1 = 100.0F;
-            float width2 = 900.0F;
-            float height2 = 250.0F;
-            RectangleF drawRect1 = new RectangleF(x1, y1, width2, height2);
+        // Create rectangle for drawing.
+        float x1 = 0.0F;
+        float y1 = 100.0F;
+        float width2 = 900.0F;
+        float height2 = 250.0F;
+        RectangleF drawRect1 = new RectangleF(x1, y1, width2, height2);
 
-            // Draw rectangle to screen.
-            Pen blackPen1 = new Pen(Color.Black);
-            e.Graphics.DrawRectangle(blackPen1, x1, y1, width2, height2);
+        // Draw rectangle to screen.
+        Pen blackPen1 = new Pen(Color.Black);
+        e.Graphics.DrawRectangle(blackPen1, x1, y1, width2, height2);
 
-            // Set format of string.
-            StringFormat drawFormat1 = new StringFormat();
-            drawFormat.Alignment = StringAlignment.Far;
+        // Set format of string.
+        StringFormat drawFormat1 = new StringFormat();
+        drawFormat.Alignment = StringAlignment.Far;
 
-            // Draw string to screen.
-            e.Graphics.DrawString(drawString1, drawFont1, drawBrush2, drawRect1, drawFormat1);
-
-
-
+        // Draw string to screen.
+        e.Graphics.DrawString(drawString1, drawFont1, drawBrush2, drawRect1, drawFormat1);
 
 
 
 
-            // Create string to draw.
-            String drawString3 = "BASIC RULES:" + '\n' +
-                                 "1. When you click the start button from the home screen, there will be a picture of three " + '\n' +
-                                 "    different pokemon, which are acutally buttons you can mouse-click on. One is Charizard," + '\n' +
-                                 "    a fire type, the red pokemon. Next is Venusaur, a grass type, the green pokemon. Lastly," + '\n' +
-                                 "    Blastoise, a water type, the blue pokemon. Using your knowlege of type matchups, you" + '\n' +
-                                 "    will be playing rock paper scissors against a CPU. If the CPU chooses charizard while" + '\n' +
-                                 "    you choose venusaur, you will lose! Have fun!" + '\n' +
-                                 "SPECIAL THANKS:" + '\n' +
-                                 "Carson He and Camron Wilson";
-            // Create font and brush.
-            Font drawFont4 = new Font("Arial", 16);
-            SolidBrush drawBrush3 = new SolidBrush(Color.Black);
 
-            // Create rectangle for drawing.
-            float x3 = 0.0F;
-            float y3 = 400.0F;
-            float width3 = 900.0F;
-            float height3 = 230.0F;
-            RectangleF drawRect3 = new RectangleF(x3, y3, width3, height3);
 
-            // Draw rectangle to screen.
-            Pen blackPen3 = new Pen(Color.Black);
-            e.Graphics.DrawRectangle(blackPen3, x3, y3, width3, height3);
 
-            // Set format of string.
-            StringFormat drawFormat3 = new StringFormat();
-            drawFormat.Alignment = StringAlignment.Far;
+        // Create string to draw.
+        String drawString3 = "BASIC RULES:" + '\n' +
+                             "1. When you click the start button from the home screen, there will be a picture of three " + '\n' +
+                             "    different pokemon, which are acutally buttons you can mouse-click on. One is Charizard," + '\n' +
+                             "    a fire type, the red pokemon. Next is Venusaur, a grass type, the green pokemon. Lastly," + '\n' +
+                             "    Blastoise, a water type, the blue pokemon. Using your knowlege of type matchups, you" + '\n' +
+                             "    will be playing rock paper scissors against a CPU. If the CPU chooses charizard while" + '\n' +
+                             "    you choose venusaur, you will lose! Have fun!" + '\n' +
+                             "SPECIAL THANKS:" + '\n' +
+                             "Carson He and Camron Wilson";
+        // Create font and brush.
+        Font drawFont4 = new Font("Arial", 16);
+        SolidBrush drawBrush3 = new SolidBrush(Color.Black);
 
-            // Draw string to screen.
-            e.Graphics.DrawString(drawString3, drawFont4, drawBrush3, drawRect3, drawFormat3);
+        // Create rectangle for drawing.
+        float x3 = 0.0F;
+        float y3 = 400.0F;
+        float width3 = 900.0F;
+        float height3 = 230.0F;
+        RectangleF drawRect3 = new RectangleF(x3, y3, width3, height3);
 
-        }
+        // Draw rectangle to screen.
+        Pen blackPen3 = new Pen(Color.Black);
+        e.Graphics.DrawRectangle(blackPen3, x3, y3, width3, height3);
+
+        // Set format of string.
+        StringFormat drawFormat3 = new StringFormat();
+        drawFormat.Alignment = StringAlignment.Far;
+
+        // Draw string to screen.
+        e.Graphics.DrawString(drawString3, drawFont4, drawBrush3, drawRect3, drawFormat3);
+
     }
 }
-        
+}
+
